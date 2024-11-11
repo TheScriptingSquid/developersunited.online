5288a85bac0bcd518386710650ec1697ca865417
TheScriptingSquid <squidyscripts@gmail.com>
2024-11-10 22:56:30 -0600

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
+// Hide the loading screen once the page is fully loaded
+window.addEventListener('load', function () {
+  setTimeout(function () {
+    const loadingScreen = document.getElementById('loadingScreen');
+    loadingScreen.style.opacity = 0;
+    loadingScreen.style.pointerEvents = 'none';
+  }, 2000); // Set loading duration to 2 seconds
+});
 
